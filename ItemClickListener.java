250c33af50662908799b0fdd617d01cb2016b660
Katta Prasanth Kumar <39497825+PrasanthKumarKatta@users.noreply.github.com>
2018-06-21 11:40:29 +0530

    Add files via upload

---
+++ ItemClickListener.java
@@ -0,0 +1,9 @@
+package com.kpcode4u.prasanthkumar.moviesapp.OnClickListeners;
+
+/**
+ * Created by Prasanth kumar on 05/06/2018.
+ */
+
+public interface ItemClickListener {
+    void onItemClickListener(int posi);
+}
